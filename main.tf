853d23b1f46468e113a03100bbce394d683fac6f
maatros <kosin.piotr@gmail.com>
2022-06-23 00:15:13 +0200

    literówka

--- main.tf
+++ main.tf
@@ -3,32 +3,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "docker-project-bucket"
-  # Enable versioning so we can see the full revision history of our state files
-  versioning {
-    enabled = true
-  }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "docker-project-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 data "aws_availability_zones" "available_zones" {
   state = "available"
 }
@@ -217,7 +191,7 @@ terraform {
     # Replace this with your bucket name!
     bucket         = "docker-project-bucket"
     key            = "global/s3/terraform.tfstate"
-    region         = "us-east-2"
+    region         = "us-east-1"
     # Replace this with your DynamoDB table name!
     dynamodb_table = "docker-project-locks"
     encrypt        = true
