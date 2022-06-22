994a12ad6f370e5180ae0a7b5ebbf99ddbd2416e
maatros <kosin.piotr@gmail.com>
2022-06-23 00:01:29 +0200

    merged tf files

--- main.tf
+++ main.tf
@@ -1,3 +1,34 @@
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "docker-project-bucket"
+  # Enable versioning so we can see the full revision history of our state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "docker-project-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
@@ -7,12 +38,8 @@ terraform {
     # Replace this with your DynamoDB table name!
     dynamodb_table = "docker-project-locks"
     encrypt        = true
+    depends_on = [aws_s3_bucket.terraform_state,aws_dynamodb_table.terraform_locks]
   }
-}
-
-# Configure the AWS provider
-provider "aws" {
-  region = "us-east-1"
 }
 
 data "aws_availability_zones" "available_zones" {
