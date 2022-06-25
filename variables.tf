6e5b994993e9f818311a8662eb08b23de4d81cc7
maatros <kosin.piotr@gmail.com>
2022-06-25 19:02:57 +0200

    rised version and added docker container

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "region" {
 #Application count - how many docker instances should start
 variable "app_count" {
   type = number
-  default = 2
+  default = 3
 }
 
 #Availability Zones - use all available
