ba62b8d18d0cc140cffcb4512b342bee9f8d4c17
maatros <kosin.piotr@gmail.com>
2022-06-28 21:31:33 +0200

    cosmetics

--- main.tf
+++ main.tf
@@ -13,15 +13,12 @@ terraform {
     encrypt        = true
   }
 }
-
 data "aws_availability_zones" "available_zones" {
   state = var.availability_zones_state
 }
-
 resource "aws_vpc" "docker_vpc" {
   cidr_block = var.docker_private_subnet_CIDR
 }
-
 resource "aws_subnet" "docker_public" {
   count                   = 2
   cidr_block              = cidrsubnet(aws_vpc.docker_vpc.cidr_block, 8, 2 + count.index)
@@ -29,7 +26,6 @@ resource "aws_subnet" "docker_public" {
   vpc_id                  = aws_vpc.docker_vpc.id
   map_public_ip_on_launch = true
 }
-
 resource "aws_subnet" "docker_private" {
   count             = 2
   cidr_block        = cidrsubnet(aws_vpc.docker_vpc.cidr_block, 8, count.index)
@@ -40,25 +36,21 @@ resource "aws_subnet" "docker_private" {
 resource "aws_internet_gateway" "docker_internet_gateway" {
   vpc_id = aws_vpc.docker_vpc.id
 }
-
 resource "aws_route" "docker_internet_access" {
   route_table_id         = aws_vpc.docker_vpc.main_route_table_id
   destination_cidr_block = var.route_destination
   gateway_id             = aws_internet_gateway.docker_internet_gateway.id
 }
-
 resource "aws_eip" "docker_gateway" {
   count      = 2
   vpc        = true
   depends_on = [aws_internet_gateway.docker_internet_gateway]
 }
-
 resource "aws_nat_gateway" "docker_gateway" {
   count         = 2
   subnet_id     = element(aws_subnet.docker_public.*.id, count.index)
   allocation_id = element(aws_eip.docker_gateway.*.id, count.index)
 }
-
 resource "aws_route_table" "docker_private" {
   count  = 2
   vpc_id = aws_vpc.docker_vpc.id
@@ -68,24 +60,20 @@ resource "aws_route_table" "docker_private" {
     nat_gateway_id = element(aws_nat_gateway.docker_gateway.*.id, count.index)
   }
 }
-
 resource "aws_route_table_association" "docker_private" {
   count          = 2
   subnet_id      = element(aws_subnet.docker_private.*.id, count.index)
   route_table_id = element(aws_route_table.docker_private.*.id, count.index)
 }
-
 resource "aws_security_group" "docker_lb_sg" {
   name        = var.docker_application_load_balancer_security_group_name
   vpc_id      = aws_vpc.docker_vpc.id
-
   ingress {
     protocol    = var.docker_application_load_balancer_security_group_ingress_protocols
     from_port   = var.docker_application_load_balancer_security_group_ingress_ports
     to_port     = var.docker_application_load_balancer_security_group_ingress_ports
     cidr_blocks = [var.docker_application_load_balancer_security_group_ingress_CIDR_blocks]
   }
-
   egress {
     from_port = var.docker_application_load_balancer_security_group_egress_ports
     to_port   = var.docker_application_load_balancer_security_group_egress_ports
@@ -93,13 +81,11 @@ resource "aws_security_group" "docker_lb_sg" {
     cidr_blocks = [var.docker_application_load_balancer_security_group_egress_CIDR_blocks]
   }
 }
-
 resource "aws_lb" "docker_lb" {
   name            = var.docker_application_load_balancer_name
   subnets         = aws_subnet.docker_public.*.id
   security_groups = [aws_security_group.docker_lb_sg.id]
 }
-
 resource "aws_lb_target_group" "hello_world" {
   name        = var.docker_application_load_balancer_target_group_name
   port        = var.docker_application_load_balancer_target_group_port
@@ -107,19 +93,15 @@ resource "aws_lb_target_group" "hello_world" {
   vpc_id      = aws_vpc.docker_vpc.id
   target_type = var.docker_application_load_balancer_target_group_target_type
 }
-
 resource "aws_lb_listener" "hello_world" {
   load_balancer_arn = aws_lb.docker_lb.id
   port              = var.docker_application_load_balancer_listener_port
   protocol          = var.docker_application_load_balancer_listener_protocol
-
   default_action {
     target_group_arn = aws_lb_target_group.hello_world.id
     type             = var.docker_application_load_balancer_listener_default_action_type
   }
 }
-
-
 # ----- This section is only for workaround purpose BEGIN -----
 data "aws_ecr_image" "aws_ecr_docker_image" {
   registry_id = 718206584555
@@ -127,7 +109,6 @@ data "aws_ecr_image" "aws_ecr_docker_image" {
   image_tag       = "latest"
   
 }
-
 resource "time_sleep" "wait_for_new_version_of_docker_image" {
   create_duration = "60s"
   triggers = {
@@ -135,7 +116,6 @@ resource "time_sleep" "wait_for_new_version_of_docker_image" {
   }
   depends_on = [aws_ecs_task_definition.hello_world]
 }
-
 # ----- This section is only for workaround purpose END -----
 resource "aws_ecs_task_definition" "hello_world" {
   family                   = var.docker_ecs_task_definition_family
@@ -162,9 +142,7 @@ resource "aws_ecs_task_definition" "hello_world" {
   }
 ]
 DEFINITION
-
 }
-
 resource "aws_security_group" "hello_world_task" {
   name        = var.docker_task_security_group_name
   vpc_id      = aws_vpc.docker_vpc.id
@@ -175,7 +153,6 @@ resource "aws_security_group" "hello_world_task" {
     to_port         = var.docker_task_security_group_ingres_port
     security_groups = [aws_security_group.docker_lb_sg.id]
   }
-
   egress {
     protocol    = var.docker_task_security_group_egress_protocol
     from_port   = var.docker_task_security_group_egress_port
@@ -183,11 +160,9 @@ resource "aws_security_group" "hello_world_task" {
     cidr_blocks = [var.docker_task_security_group_egress_CIDR_blocks]
   }
 }
-
 resource "aws_ecs_cluster" "docker_cluster" {
   name = var.docker_ecs_cluster_name
 }
-
 resource "aws_ecs_service" "hello_world_service" {
   name            = var.docker_ecs_service_name
   cluster         = aws_ecs_cluster.docker_cluster.id
@@ -200,7 +175,6 @@ resource "aws_ecs_service" "hello_world_service" {
     security_groups = [aws_security_group.hello_world_task.id]
     subnets         = aws_subnet.docker_private.*.id
   }
-
   load_balancer {
     target_group_arn = aws_lb_target_group.hello_world.id
     container_name   = var.docker_ecs_service_container_name
@@ -208,5 +182,3 @@ resource "aws_ecs_service" "hello_world_service" {
   }
   depends_on = [aws_lb_listener.hello_world,time_sleep.wait_for_new_version_of_docker_image]
 }
-
-
