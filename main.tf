592695d9f0577720a5eec7385d395e1ca6cd6701
maatros <kosin.piotr@gmail.com>
2022-07-05 11:45:38 +0200

    moved s3 task to different branch

--- main.tf
+++ main.tf
@@ -1,182 +1,35 @@
-# Configure the AWS provider
 provider "aws" {
   region = var.region
 }
-
-# Remote state configured with S3 bucket
-terraform {
-  backend "s3" {
-    bucket         = "docker-project-bucket"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "docker-project-locks"
-    encrypt        = true
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+  force_destroy = var.bucket_force_destroy
+  versioning {
+    enabled = var.bucket_versioning
   }
-}
-data "aws_availability_zones" "available_zones" {
-  state = var.availability_zones_state
-}
-resource "aws_vpc" "docker_vpc" {
-  cidr_block = var.docker_private_subnet_CIDR
-}
-resource "aws_subnet" "docker_public" {
-  count                   = 2
-  cidr_block              = cidrsubnet(aws_vpc.docker_vpc.cidr_block, 8, 2 + count.index)
-  availability_zone       = data.aws_availability_zones.available_zones.names[count.index]
-  vpc_id                  = aws_vpc.docker_vpc.id
-  map_public_ip_on_launch = true
-}
-resource "aws_subnet" "docker_private" {
-  count             = 2
-  cidr_block        = cidrsubnet(aws_vpc.docker_vpc.cidr_block, 8, count.index)
-  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
-  vpc_id            = aws_vpc.docker_vpc.id
-}
-resource "aws_internet_gateway" "docker_internet_gateway" {
-  vpc_id = aws_vpc.docker_vpc.id
-}
-resource "aws_route" "docker_internet_access" {
-  route_table_id         = aws_vpc.docker_vpc.main_route_table_id
-  destination_cidr_block = var.route_destination
-  gateway_id             = aws_internet_gateway.docker_internet_gateway.id
-}
-resource "aws_eip" "docker_gateway" {
-  count      = 2
-  vpc        = true
-  depends_on = [aws_internet_gateway.docker_internet_gateway]
-}
-resource "aws_nat_gateway" "docker_gateway" {
-  count         = 2
-  subnet_id     = element(aws_subnet.docker_public.*.id, count.index)
-  allocation_id = element(aws_eip.docker_gateway.*.id, count.index)
-}
-resource "aws_route_table" "docker_private" {
-  count  = 2
-  vpc_id = aws_vpc.docker_vpc.id
-  route {
-    cidr_block = var.route_destination
-    nat_gateway_id = element(aws_nat_gateway.docker_gateway.*.id, count.index)
-  }
-}
-resource "aws_route_table_association" "docker_private" {
-  count          = 2
-  subnet_id      = element(aws_subnet.docker_private.*.id, count.index)
-  route_table_id = element(aws_route_table.docker_private.*.id, count.index)
-}
-resource "aws_security_group" "docker_lb_sg" {
-  name        = var.docker_application_load_balancer_security_group_name
-  vpc_id      = aws_vpc.docker_vpc.id
-  ingress {
-    protocol    = var.docker_application_load_balancer_security_group_ingress_protocols
-    from_port   = var.docker_application_load_balancer_security_group_ingress_ports
-    to_port     = var.docker_application_load_balancer_security_group_ingress_ports
-    cidr_blocks = [var.docker_application_load_balancer_security_group_ingress_CIDR_blocks]
-  }
-  egress {
-    from_port = var.docker_application_load_balancer_security_group_egress_ports
-    to_port   = var.docker_application_load_balancer_security_group_egress_ports
-    protocol  = var.docker_application_load_balancer_security_group_egress_protocols
-    cidr_blocks = [var.docker_application_load_balancer_security_group_egress_CIDR_blocks]
-  }
-}
-resource "aws_lb" "docker_lb" {
-  name            = var.docker_application_load_balancer_name
-  subnets         = aws_subnet.docker_public.*.id
-  security_groups = [aws_security_group.docker_lb_sg.id]
-}
-resource "aws_lb_target_group" "hello_world" {
-  name        = var.docker_application_load_balancer_target_group_name
-  port        = var.docker_application_load_balancer_target_group_port
-  protocol    = var.docker_application_load_balancer_target_group_protocol
-  vpc_id      = aws_vpc.docker_vpc.id
-  target_type = var.docker_application_load_balancer_target_group_target_type
-}
-resource "aws_lb_listener" "hello_world" {
-  load_balancer_arn = aws_lb.docker_lb.id
-  port              = var.docker_application_load_balancer_listener_port
-  protocol          = var.docker_application_load_balancer_listener_protocol
-  default_action {
-    target_group_arn = aws_lb_target_group.hello_world.id
-    type             = var.docker_application_load_balancer_listener_default_action_type
-  }
-}
-# ----- Workaround for new docker image BEGIN -----
-data "aws_ecr_image" "aws_ecr_docker_image" {
-  registry_id = 718206584555
-  repository_name = "from-git-repository"
-  image_tag       = "latest"
-}
-# ----- Workaround for new docker image END -----
-resource "aws_ecs_task_definition" "hello_world" {
-  family                   = var.docker_ecs_task_definition_family
-  network_mode             = var.docker_ecs_task_definition_network_mode
-  requires_compatibilities = [var.docker_ecs_task_definition_requires_compatibilities]
-  cpu                      = var.docker_ecs_task_definition_cpu
-  memory                   = var.docker_ecs_task_definition_memory
-  execution_role_arn       = var.task_execution_role
-  container_definitions = <<DEFINITION
-[
-  {
-    "image": "718206584555.dkr.ecr.us-east-1.amazonaws.com/from-git-repository:latest@${data.aws_ecr_image.aws_ecr_docker_image.image_digest}",
-    "cpu": 1024,
-    "memory": 2048,
-    "essential" : true,
-    "name": "simple-container",
-    "networkMode": "awsvpc",
-    "portMappings": [
-      {
-        "containerPort": 80,
-        "hostPort": 80
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = var.bucket_encryption_algorithm
       }
-    ]
+    }
   }
-]
-DEFINITION
 }
-# ----- Workaround for Github Action Docker image build time BEGIN -----
-resource "time_sleep" "wait_for_new_version_of_docker_image" {
-  create_duration = "60s"
-  triggers = {
-    "task_definition" = aws_ecs_task_definition.hello_world.arn
-  }
-  depends_on = [aws_ecs_task_definition.hello_world]
-}
-# ----- Workaround for Github Action Docker image build time END -----
-resource "aws_security_group" "hello_world_task" {
-  name        = var.docker_task_security_group_name
-  vpc_id      = aws_vpc.docker_vpc.id
 
-  ingress {
-    protocol        = var.docker_task_security_group_ingresss_protocol
-    from_port       = var.docker_task_security_group_ingres_port
-    to_port         = var.docker_task_security_group_ingres_port
-    security_groups = [aws_security_group.docker_lb_sg.id]
-  }
-  egress {
-    protocol    = var.docker_task_security_group_egress_protocol
-    from_port   = var.docker_task_security_group_egress_port
-    to_port     = var.docker_task_security_group_egress_port
-    cidr_blocks = [var.docker_task_security_group_egress_CIDR_blocks]
-  }
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls   = var.bucket_public_access_block_public_acls
+  block_public_policy = var.bucket_public_access_block_public_policy
+  ignore_public_acls = var.bucket_public_access_ignore_public_acls
+  restrict_public_buckets =  var.bucket_public_access_restrict_public_buckets
 }
-resource "aws_ecs_cluster" "docker_cluster" {
-  name = var.docker_ecs_cluster_name
-}
-resource "aws_ecs_service" "hello_world_service" {
-  name            = var.docker_ecs_service_name
-  cluster         = aws_ecs_cluster.docker_cluster.id
- #task_definition = aws_ecs_task_definition.hello_world.arn
-  task_definition = time_sleep.wait_for_new_version_of_docker_image.triggers["task_definition"]
-  desired_count   = var.app_count
-  launch_type     = var.docker_ecs_service_launch_type
-  network_configuration {
-    security_groups = [aws_security_group.hello_world_task.id]
-    subnets         = aws_subnet.docker_private.*.id
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.dynamodb_table_name
+  billing_mode = var.dynamodb_table_billing_mode
+  hash_key     = var.dynamodb_table_hash_key
+  attribute {
+    name = var.dynamodb_table_attribute_name
+    type = var.dynamodb_table_attribute_type
   }
-  load_balancer {
-    target_group_arn = aws_lb_target_group.hello_world.id
-    container_name   = var.docker_ecs_service_container_name
-    container_port   = var.docker_ecs_service_container_port
-  }
-  depends_on = [aws_lb_listener.hello_world,time_sleep.wait_for_new_version_of_docker_image]
 }
